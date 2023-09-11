63a0f1f257207a33c218b493468768a21799520d
claudiodantas <claudiodantas1996@gmail.com>
2023-09-11 16:41:43 -0300

    add styling

--- src/components/GameSummary.tsx
+++ src/components/GameSummary.tsx
@@ -14,16 +14,29 @@ export default function GameSummary({
   return (
     <>
       <p>
-        Vpcê acertou {getNumOfCorrectAnswers()} de um total de{" "}
-        {questions.length} questões
+        Você acertou {getNumOfCorrectAnswers()} de um total de{" "}
+        {questions.length} questões!
       </p>
-      {gameSummary.map((data: Summary, i: number) => (
-        <div key={i} className="flex flex-col items-center">
-          <p>{data.question}</p>
-          <p>Resposta coreta{data.correctAnswer.answer}</p>
-          <p>Resposta do jogador{data.chosenAnswer.answer}</p>
-        </div>
-      ))}
+      <div className=" px-[20px]">
+        {gameSummary.map((data: Summary, i: number) => (
+          <div key={i} className="flex my-[20px] text-sm md:text-base">
+            <span
+              className={`h-[40px] w-[40px] ${
+                data.chosenAnswer.isCorrect ? "bg-green-500" : "bg-red-500"
+              } rounded-full flex font-bold items-center justify-center`}
+            >
+              {i + 1}
+            </span>
+            <div className="ml-[10px]">
+              <h4 className="w-[250px] sm:w-[400px] lg:w-[500px] xl:w-[650px]">
+                {data.question}
+              </h4>
+              <h6 className="opacity-50">{data.correctAnswer.answer}</h6>
+              <h6>{data.chosenAnswer.answer}</h6>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
